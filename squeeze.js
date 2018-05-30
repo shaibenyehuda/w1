fba358e86c878a2efdec1827546f3c413ed6dc75
Shai Ben-Yehuda <shaiby@Temp-Mac-15.local>
2018-05-30 18:44:57 +0300

    clustering

--- squeeze.js
+++ squeeze.js
@@ -6,6 +6,31 @@ const { map, filter, flatMap, take, toArray } = require('rxjs/operators');
 require('./browser/siteAsJson.js');
 require('./browser/rendererModel.js');
 
+const paramDrivers = {
+    StyledText: ['text'],
+    LinkableButton: { 
+        get: (dataObj,page) => {
+            const anchorLink = page.data.document_data[dataObj.link.slice(1)];
+            return {
+                label: dataObj.label,
+                type: dataObj.type,
+                anchorName: anchorLink.anchorName,
+                pageId: anchorLink.pageId
+            }
+        }
+    },
+    TPAWidget: { 
+        get: (dataObj,page,comp) => {
+            const appData = jsonpath.query(rendererModel,`$..*[?(@.applicationId=="${dataObj.applicationId}")]`)[0];
+            const instance = appData.instance;
+            const url = `http://progallery.wix.com/gallery.html?compId=${comp.id}&instance=${instance}`;
+            return {
+                instance,url
+            }
+        }
+    },
+}
+
 function galleryImages(tpa) {
     if (!tpa || !tpa.dataQuery || !tpa.dataQuery.slice) return 'err';
     var app = jsonpath.query(siteAsJson,`$..*[?(@.id=="${tpa.dataQuery.slice(1)}")]`)[0].applicationId;
@@ -32,6 +57,7 @@ var items = jsonpath.apply(siteAsJson,widgets,x=>x)
     .map(({path,value})=>({path,value,id:value.id,title:siteAsJson.pages[path[2]].title}));
 
 //fetchCategory("Free Cars","d5df13be-7e25-4498-9da3-9eceb9511339");
+//fetchCategory("Free Fruit","1ee1213d-a061-4b86-92e2-ddb12eb84d51");
 
 function fetchCategory(title,id) {
     from(items.filter(x=>x.title == title)).pipe(flatMap(x=>galleryImages(x.value)),filter(x=>x),flatMap(x=>x.items),toArray()).subscribe(items=>
@@ -43,6 +69,7 @@ function fetchCategory(title,id) {
         }).join('\n'))
     )    
 }
+
 function leafs(obj) {
     let res = [];
     function doLeafs(inner,path) {
@@ -66,20 +93,16 @@ function clusteringProps(p) {
     return { ar: Array.from(s), s, comps: p.structure.components}
 }
 
-function sameCluster(p1,p2) {
-    const diffs = { d1: p1.ar.filter(x=>!p2.s.has(x)), d2: p2.ar.filter(x=>!p1.s.has(x)) };
-    return (diffs.d1.length == 0 || diffs.d2.length == 0)
-}
-
 function cluster(pages) {
     let clusters = [];
     pages.forEach((page,i)=>{
         if (page.cluster) return;
         let cluster = [page];
+        cluster.title = page.value.title;
         page.cluster = cluster;
         clusters.push(cluster);
         pages.slice(i).forEach(toCompare=>{
-            if (toCompare.ar.filter(x=>!page.s.has(x)).length == 0) {
+            if (!toCompare.cluster && toCompare.ar.filter(x=>!page.s.has(x)).length == 0) {
                 cluster.push(toCompare);
                 toCompare.cluster = cluster;
             }
@@ -88,21 +111,42 @@ function cluster(pages) {
     return clusters.filter(cl=>cl.length > 1).sort((p1,p2)=>p2.length-p1.length);
 }
 
-function paramsOfCluster(cl) {
-    const paths = cl[0].ar;
-    paths.forEach(path=>{
-        const props = new Set([].concat.apply([],cl.map(item=>Object.getOwnPropertyNames(pathToObj(item.path)))) )
-    })
+function applyPath(obj,path) {
+    const pathAr = path.split('$.').pop().split('#')[0].split('.');
+    var ret = pathAr.reduce((o,p)=>o[p],obj);
+    if (!ret)
+        console.log('null val for path ' + path);
+    return ret || {};
+}
 
+function calcParamsOfCluster(cl) {
+    const pathsOfObjs = cl[0].ar;
+    cl.params = pathsOfObjs
+        .filter(path=>applyPath(cl[0].comps,path).dataQuery)
+        .map(path=>{ 
+            let comp = applyPath(cl[0].comps,path);
+            let dataObj = cl[0].value.data.document_data[comp.dataQuery.slice(1)];
+            return {type: dataObj.type, app: dataObj.applicationId, comp, path, dataObj }
+        }).map(e=>Object.assign(e,paramsOfType(e.type,e.dataObj,cl[0].value,e.comp)))
 }
 
 let clusters = cluster(pages);
-//console.log(clusters.map(cl=>`${cl.length}: ${cl[0].ar.length}-${cl.slice(-1)[0].ar.length}`))
+calcParamsOfCluster(clusters[0]);
+console.log(clusters[0].params);
+console.log(clusters.map(cl=>`${cl.length}: ${cl[0].ar.length}-${cl.slice(-1)[0].ar.length}`))
 const pageCoverage = clusters.reduce((sum,cl)=>cl.length+sum,0)
 console.log('Coverage: ' + pageCoverage + ' of ' +pages.length)
 //console.log(pages.map(x=>x.ar.length).join(','))
 //sameCluster(pages[0],pages[1]));
 
-
-
+function paramsOfType(type,dataObj,page,comp) {
+    const driver = paramDrivers[type];
+    if (Array.isArray(driver)) {
+        var ret = {};
+        driver.forEach(p=>ret[p] = dataObj[p])
+        return ret;
+    }
+    if (typeof driver == 'object' && driver.get)
+        return driver.get(dataObj,page,comp)
+}
 
